a537a05bc519f4bbecf2465046f49ee2c29e199a
w3schools-test <subhajitincareer@gmail.com>
2025-10-28 20:04:41 +0530

    server start in backend

---
+++ server/src/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// 🧩 ES module compatible __dirname তৈরি
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+// ✅ Parse JSON
+app.use(express.json());
+
+// ✅ Serve client dist files
+const distPath = path.join(__dirname, "../../client/dist");
+app.use(express.static(distPath));
+
+// ✅ API routes (example)
+app.get("/api", (req, res) => {
+    res.json({ message: "🎬 API working fine!" });
+});
+;
+
+// ✅ Start server
+app.listen(PORT, () => {
+    console.log(`✅ Server running on http://localhost:${PORT}`);
+});
